3080f5ef779006754890f7d5e844f5425e28c9c8
despresj <54477023+despresj@users.noreply.github.com>
2020-11-04 11:53:18 -0500

    added model

--- R/cleaningscript.R
+++ R/cleaningscript.R
@@ -8,19 +8,18 @@ cleaning <- function(df){
   df <- pivot_longer(df, cols = -country, names_to = "year")
 }
 
-files %>% 
+data <- files %>% 
   map(function(x) read_csv(paste0("raw_data/", x))) %>% 
   setNames(gsub("\\.csv$", "", files)) %>% 
   map(cleaning) %>% 
   bind_rows(.id = "id") %>% 
-  pivot_wider(names_from = id) # %>% 
-write_csv(here::here("data", "df.csv"))
+  pivot_wider(names_from = id) 
+
+
 
 # Filtering ---------------------------------------------------------------
-data <- read_csv(here::here("data", "df.csv")) %>% 
-  filter(year > 2009) 
+data %>% filter(year > 2000) %>% 
+write_csv(here::here("data", "df.csv"))
 
-data %>% View()
-
-dim(data)
+data <- read_csv(here::here("data", "df.csv")) 
 
