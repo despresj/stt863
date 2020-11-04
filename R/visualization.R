ace54f000a8648046864e3dba67f348c8553e0ef
despresj <despresj@mail.gvsu.edu>
2020-11-03 21:19:44 -0500

    ran some plots

--- R/visualization.R
+++ R/visualization.R
@@ -1,19 +1,20 @@
 library(tidyverse)
-
 # Visuals -------------------------------------------------------------
+theme_set(theme_light())
 
 data <- read_csv(here::here("data", "df.csv"))
+best_subsets <- read_csv(here::here("data", "best_subsets.csv"))
 
-data %>% 
+corr_matrix <- data %>% 
   select(-year) %>% 
   select_if(is.numeric) %>% 
-  cor(.,  use = "complete.obs") %>% 
-  View()
+  cor(.,  use = "complete.obs")
+
 
 data %>% 
   filter(year > 2015) %>% 
   ggplot(mapping = aes(x = freedix_fh)) + 
-  geom_histogram()
+  geom_histogram(bins = 10)
 
 data %>% 
   filter(year > 2015) %>% 
@@ -33,18 +34,52 @@ data %>%
 
 data %>% 
   filter(year > 2015) %>% 
-  #  mutate(trans_civlib = gini) %>% 
   ggplot(mapping = aes(x = gini)) + 
   geom_histogram()
 
 data %>% 
-  select(country, gini) %>% 
-  group_by(country) %>%
-  summarise(mean = mean(gini, na.rm = TRUE)) %>% 
-  View()
+  ggplot(aes(x = gini, y = polrights_fh)) +
+  geom_hex(binwidth = c(3, .6)) + 
+  scale_fill_gradient(low = "#7FFFD4", high = "#8B8378") + 
+  geom_smooth(method = "lm", se = TRUE, color = "red") +
+  geom_point(data = data %>% 
+               filter(country == "United States", 
+                         year == 2018), 
+      aes(x = gini, y = polrights_fh), size = 5, color = "red") +
+  labs(x = "GINI Coeffeceint", y = "Political Rights")
+
 
 data %>% 
-  select(country, journakilled) %>% 
-  group_by(country) %>%
-  summarise(mean = mean(journakilled, na.rm = TRUE)) %>% 
-  View()
+  ggplot(aes(x = gini, y = polrights_fh)) + 
+  geom_smooth(se = false)
+
+r <- best_subsets %>% group_by(df) %>% summarise(adj = max(adj.r.squared, na.rm = T))
+
+best_subsets %>% 
+  ggplot(aes(x = df, y = adj.r.squared)) + 
+  geom_point() + 
+  geom_smooth(data = r, aes(df, adj), se = FALSE, span = 0.5, color = "Green") + 
+  labs(title = "Best Subsets", x = "Parameters", y = "Adjusted R Squared")
+
+AIC <- best_subsets %>% group_by(df) %>% summarise(AIC = max(AIC, na.rm = T))
+
+best_subsets %>% 
+  ggplot(aes(x = df, y = AIC)) + 
+  geom_point() + 
+  geom_smooth(data = AIC, aes(df, AIC), se = FALSE, span = .5, color = "Green") + 
+  labs(title = "Best Subsets", x = "Parameters", y = "AIC")
+
+model_df4 <- best_subsets %>% filter(df == 4) %>% 
+  arrange(desc(adj.r.squared)) %>% 
+  select(model) %>% head(1) %>% as.character()
+
+fit <- lm(model_df4, data = data)
+fit %>% broom::tidy()
+
+model_df3 <- best_subsets %>% filter(df == 3) %>% 
+  arrange(desc(adj.r.squared)) %>% 
+  select(model) %>% head(1) %>% as.character()
+
+fit <- lm(model_df3, data = data)
+fit %>% broom::tidy()
+
