557a3f7682dcb74dc46808e66f0a9db213bcf1c1
despresj <despresj@mail.gvsu.edu>
2020-11-12 21:23:24 -0500

    plots and plots

--- R/regression.R
+++ R/regression.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(kableExtra)
+library(patchwork)
 
 data <- readr::read_csv(here::here("data", "df.csv"))
 subsets <- readr::read_csv(here::here("data", "subsets.csv"))
@@ -46,3 +48,36 @@ fit_df6 <- lm(model_df6_formula, data)
 
 summary(fit_df6)
 
+
+
+# model stats -------------------------------------------------------------
+
+
+# remedial measures -------------------------------------------------------------------
+outliars <- model_df5 %>% 
+  broom::augment() %>% 
+  filter(.resid < -4*sd(.resid))
+
+hist <- model_df5 %>% 
+  broom::augment() %>% 
+  ggplot(aes(.resid)) + 
+  geom_histogram(bins = 100, fill = "black") +
+  scale_x_continuous(limits = c(-4,4)) 
+hist
+qq <- model_df5 %>% 
+  broom::augment() %>% 
+  ggplot(aes(sample = .resid)) + 
+  stat_qq() +
+  stat_qq_line() + 
+scale_x_continuous(limits = c(-4,4)) + 
+scale_y_continuous(limits = c(-4,4)) 
+
+
+
+labeled_outliars <- hist +
+  geom_point(outliars, mapping = aes(x = .resid, y = 1), color = "#D7261E", shape = 13, size = 10) + 
+  geom_label(outliars, mapping = aes(x = .resid, y = 10, label = "OUTLIARS"), color = "#D7261E", size = 3)
+
+labeled_qq <- qq + geom_point(outliars, mapping = aes(x = -3, y = -3.9), color = "#D7261E", shape = 0, size = 20) 
+
+labeled_qq 
