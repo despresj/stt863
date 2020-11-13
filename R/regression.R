c064b20dc7a3f70b9fd096e919c3c3cb3e4f1456
despresj <54477023+despresj@users.noreply.github.com>
2020-11-12 23:05:01 -0500

    added plots getting there

--- R/regression.R
+++ R/regression.R
@@ -11,21 +11,9 @@ model_df5_formula <- subsets %>% filter(df == 5) %>%
   arrange(desc(adj.r.squared)) %>% 
   select(model) %>% head(1) %>% as.character()
 
-model_df5 <- lm(polrights_fh ~ corruption_perception_index_cpi + 
-            edu_exp_gdp_per_person + 
-            life_expectancy_years + 
-            military_spending_pct_of_gdp + 
-            murder_per_mil_people, data)
-
-subsets %>% filter(df == 9) %>% 
-  arrange(desc(adj.r.squared)) %>% 
-  select(model) %>% head(1) %>% as.character()
-
+fit_df5 <- lm(model_df5_formula, data)
 
 # Second model ------------------------------------------------------------
-
-subsets %>% filter(df == 4) %>% 
-  arrange(desc(adj.r.squared)) 
 
 model_df4_formula <- subsets %>% filter(df == 4) %>% 
   arrange(desc(adj.r.squared)) %>% 
@@ -33,12 +21,13 @@ model_df4_formula <- subsets %>% filter(df == 4) %>%
 
 fit_df4 <- lm(model_df4_formula, data)
 
-summary(fit_df4)
+model_df3_formula <- subsets %>% filter(df == 3) %>% 
+  arrange(desc(adj.r.squared)) %>% 
+  select(model) %>% head(1) %>% as.character() 
+
+fit_df3 <- lm(model_df3_formula, data)
 
 # third -------------------------------------------------------------------
-
-subsets %>% filter(df == 6) %>% 
-  arrange(desc(adj.r.squared)) 
 
 model_df6_formula <- subsets %>% filter(df == 6) %>% 
   arrange(desc(adj.r.squared)) %>% 
@@ -46,25 +35,21 @@ model_df6_formula <- subsets %>% filter(df == 6) %>%
 
 fit_df6 <- lm(model_df6_formula, data)
 
-summary(fit_df6)
-
-
-
-# model stats -------------------------------------------------------------
-
-
 # remedial measures -------------------------------------------------------------------
-outliars <- model_df5 %>% 
+
+outliars <- fit_df5 %>% 
   broom::augment() %>% 
   filter(.resid < -4*sd(.resid))
 
-hist <- model_df5 %>% 
+hist <- fit_df5 %>% 
   broom::augment() %>% 
   ggplot(aes(.resid)) + 
   geom_histogram(bins = 100, fill = "black") +
   scale_x_continuous(limits = c(-4,4)) 
+
 hist
-qq <- model_df5 %>% 
+
+qq <- fit_df5 %>% 
   broom::augment() %>% 
   ggplot(aes(sample = .resid)) + 
   stat_qq() +
@@ -72,12 +57,16 @@ qq <- model_df5 %>%
 scale_x_continuous(limits = c(-4,4)) + 
 scale_y_continuous(limits = c(-4,4)) 
 
-
-
 labeled_outliars <- hist +
   geom_point(outliars, mapping = aes(x = .resid, y = 1), color = "#D7261E", shape = 13, size = 10) + 
-  geom_label(outliars, mapping = aes(x = .resid, y = 10, label = "OUTLIARS"), color = "#D7261E", size = 3)
+  geom_label(outliars, mapping = aes(x = -3.3, y = 5, label = "OUTLIARS"), color = "#D7261E", size = 3)
 
-labeled_qq <- qq + geom_point(outliars, mapping = aes(x = -3, y = -3.9), color = "#D7261E", shape = 0, size = 20) 
+labeled_outliars
+
+labeled_qq <- qq + geom_point(outliars, mapping = aes(x = -3, y = -3.9), color = "#D7261E", shape = 0, size = 15) +
+                   geom_point(outliars, mapping = aes(x = -2.6, y = -2.7), color = "#D7261E", shape = 0, size = 15) +
+                   geom_label(outliars, mapping = aes(x = -3, y = -3.5, label = "OUTLIARS"), color = "#D7261E", size = 3)
+
 
 labeled_qq 
+
